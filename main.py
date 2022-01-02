45e3cbec3f816c46814e1710c35472580891e9f1
André Viana <andrebermudesviana@gmail.com>
2022-01-02 16:17:40 -0300

    added dataclass

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import controle
+from dataclasses import dataclass
+
+@dataclass
+class Individuo:
+    Kp: float
+    Ki: float
+    Kd: float
+    fitness: float
 
 # pega a nota desse tuning (quanto menor, melhor)
 Kp = 2
 Ki = 1
 Kd = 0.8
-nota = controle.test_tuning(Kp,Ki,Kd)
+individuo = Individuo(Kp, Ki, Kd, None)
+individuo.fitness = controle.test_tuning(individuo.Kp, individuo.Ki, individuo.Kd)
